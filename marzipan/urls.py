d86b542d98836384279ef261b42e37acbf53b65e
kirillherz <kirill.herz@gmail.com>
2016-01-03 18:46:07 +0300

    Добавлено представление listing_cakes и шаблон list_cakes.html

--- marzipan/urls.py
+++ marzipan/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 from photogallery.views import *
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from .settings import MEDIA_ROOT,MEDIA_URL
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+	url(r'^cakes/',listing_cakes)
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
--- photogallery/views.py
+++ photogallery/views.py
@@ -1,2 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from .models import CakeModel
+from django.shortcuts import render
 
+def listing_cakes(request):
+	cakes_list = CakeModel.objects.all()
+	paginator = Paginator(cakes_list,1)
+	page = request.GET.get('page')
+	try:
+		cakes = paginator.page(page);
+	except PageNotAnInteger:
+		cakes = paginator.page(1)
+	except EmptyPage:
+		cakes = paginator.page(paginator.num_pages)
+	return render(request,'list_cakes.html',{'cakes':cakes})
