4aecd546613828161ddcbba39b88007fde0214ad
kirillherz <kirill.herz@gmail.com>
2016-01-05 16:29:13 +0300

    Добавлены модели FeedbackModel и ReviewModel

--- photogallery/models.py
+++ photogallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 
 class CakeModel(models.Model):
 	name = models.CharField(max_length = 40, verbose_name = 'Название')
@@ -8,11 +9,34 @@ class CakeModel(models.Model):
 	cost = models.FloatField(verbose_name = 'Стоимость')
 	image = models.FileField(upload_to = 'cakes', verbose_name = 'Изображение')
 	
-	
 	class Meta:
 		verbose_name = 'Торт'
 		verbose_name_plural = 'Торты'
 		
 	def __str__(self):
 		return self.name
+
+class FeedbackModel(models.Model):
+	name = models.CharField(max_length = 40, verbose_name = 'Имя')
+	e_mail = models.EmailField(blank = True)
+	phone_number = models.IntegerField(validators = [RegexValidator("[0-9]{10}", "Incorrectly phone number" )])
+	message = models.CharField(max_length = 200)
+	
+	def __str__(self):
+		return self.name
+	
+class ReviewModel(models.Model):
+	name = models.CharField(max_length = 40, verbose_name = 'Имя')
+	cake = models.ForeignKey(CakeModel)
+	review = models.CharField(max_length = 100)
+	rating = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
+	
+	class Meta:
+		verbose_name = 'Отзыв'
+		verbose_name_plural = 'Отзывы'
 		
+	def __str__(self):
+		return self.name
+	
+
+	
