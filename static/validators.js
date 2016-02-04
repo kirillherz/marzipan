76b724123012a7c266b76b5865bfba8e9a0bf4ab
kirillherz <kirill.herz@gmail.com>
2016-02-04 17:47:12 +0300

    Добавлены валидаторы e-mail, исправлены валидаторы чисел, добавил валидатор url

--- static/validators.js
+++ static/validators.js
@@ -1,10 +1,24 @@
 var Validators = {};
 Validators.int = function (value) {
-    return isNaN(parseInt(value));
+    var pattern = "^[0-9]+$";
+    return value.match(pattern) !== null;
 };
 Validators.float = function (value) {
-    return isNaN(parseFloat(value));
+    var pattern = "^[0-9]+[.,]{1}[0-9]*$";
+    return value.match(pattern) !== null;
 };
+Validators.e_mail = function (value) {
+    var pattern = "^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$";
+    return value.match(pattern) !== null;
+};
+Validators.url = function(value){
+    var pattern = "^((https?|ftp)\:\/\/)?([a-z0-9]{1})((\.[a-z0-9-])|([a-z0-9-]))*\.([a-z]{2,6})(\/?)$";
+    return value.match(pattern) !== null;
+};
+Validators.required = function(value){
+    return value.length > 0;
+};
+
 
 function showError(input) {
     var data_validator_id = input.getAttribute("data-validator-id");
@@ -26,10 +40,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 var data_validator_value = inputs[j].getAttribute("data-validator");
                 if (data_validator_value in Validators) {
                     if (Validators[data_validator_value](inputs[j].value)) {
+                        resetError(inputs[j]);
+                    } else {
                         showError(inputs[j]);
                         event.preventDefault();
-                    } else {
-                        resetError(inputs[j]);
                     }
                 }
             }
