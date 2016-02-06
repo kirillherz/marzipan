7395209fc97b806f62aec423366406277858211a
kirillherz <kirill.herz@gmail.com>
2016-02-06 17:25:57 +0300

    Исправлен модуль валидации.

--- static/validators.js
+++ static/validators.js
@@ -11,30 +11,40 @@ Validators.e_mail = function (value) {
     var pattern = "^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$";
     return value.match(pattern) !== null;
 };
-Validators.url = function(value){
+Validators.url = function (value) {
     var pattern = "^((https?|ftp)\:\/\/)?([a-z0-9]{1})((\.[a-z0-9-])|([a-z0-9-]))*\.([a-z]{2,6})(\/?)$";
     return value.match(pattern) !== null;
 };
-Validators.required = function(value){
-    return value.length > 0;
+Validators.required = function (value) {
+    return value.trim().length > 0;
 };
 
 
 function showError(input) {
     var data_validator_id = input.getAttribute("data-validator-id");
-    var element = document.querySelector("[data-validator-id = '" + data_validator_id + "']");
-    element.style.display = "block";
+    var elements = document.querySelectorAll("[data-validator-id = '" + data_validator_id + "']");
+    for (var i = 0; i != elements.length; i++) {
+        var attribute = elements[i].getAttribute("data-class-error");
+        if (attribute !== null) {
+            elements[i].setAttribute("class", attribute);
+        }
+    }
 }
 function resetError(input) {
     var data_validator_id = input.getAttribute("data-validator-id");
-    var element = document.querySelector("[data-validator-id = '" + data_validator_id + "']");
-    element.style.display = "none";
+    var elements = document.querySelectorAll("[data-validator-id = '" + data_validator_id + "']");
+    for (var i = 0; i != elements.length; i++) {
+        var attribute = elements[i].getAttribute("data-class-reset");
+        if (attribute !== null) {
+            elements[i].setAttribute("class", attribute);
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     var forms = document.getElementsByTagName("form");
     for (var i = 0; i !== forms.length; i++) {
-        forms[i].addEventListener("submit", function () {
+        forms[i].addEventListener("submit", function (event) {
             var inputs = this.querySelectorAll("[data-validator]");
             for (var j = 0; j !== inputs.length; j++) {
                 var data_validator_value = inputs[j].getAttribute("data-validator");
