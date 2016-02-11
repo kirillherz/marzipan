aa5eaeeddad9809fd58cf1980d3b95936dbec681
kirillherz <kirill.herz@gmail.com>
2016-02-11 14:10:51 +0300

    Имена функций и переменных приведены к единому виду

--- static/validators.js
+++ static/validators.js
@@ -19,7 +19,7 @@ Validators.required = function (value) {
     return value.trim().length > 0;
 };
 
-function apply_class() {
+function applyClass() {
     var elements = document.querySelectorAll("[data-validator-id]");
     for (var i = 0; i != elements.length; i++) {
         var attribute = elements[i].getAttribute("data-class-reset");
@@ -28,8 +28,8 @@ function apply_class() {
 }
 
 function showError(input) {
-    var data_validator_id = input.getAttribute("data-validator-id");
-    var elements = document.querySelectorAll("[data-validator-id = '" + data_validator_id + "']");
+    var dataValidatorId = input.getAttribute("data-validator-id");
+    var elements = document.querySelectorAll("[data-validator-id = '" + dataValidatorId + "']");
     for (var i = 0; i != elements.length; i++) {
         var attribute = elements[i].getAttribute("data-class-error");
         if (attribute !== null) {
@@ -39,8 +39,8 @@ function showError(input) {
 }
 
 function resetError(input) {
-    var data_validator_id = input.getAttribute("data-validator-id");
-    var elements = document.querySelectorAll("[data-validator-id = '" + data_validator_id + "']");
+    var dataValidatorId = input.getAttribute("data-validator-id");
+    var elements = document.querySelectorAll("[data-validator-id = '" + dataValidatorId + "']");
     for (var i = 0; i != elements.length; i++) {
         var attribute = elements[i].getAttribute("data-class-reset");
         if (attribute !== null) {
@@ -49,7 +49,7 @@ function resetError(input) {
     }
 }
 
-function is_blank_validate(input) {
+function isBlankValidate(input) {
     var attribute = input.getAttribute("data-blank");
     if (attribute === "true" && input.value.trim().length === 0 && attribute !== null) {
         return true;
@@ -59,18 +59,18 @@ function is_blank_validate(input) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    apply_class();
+    applyClass();
     var forms = document.getElementsByTagName("form");
     for (var i = 0; i !== forms.length; i++) {
         forms[i].addEventListener("submit", function (event) {
             var inputs = this.querySelectorAll("[data-blank],[data-validator]");
             for (var j = 0; j !== inputs.length; j++) {
-                var data_validator_value = inputs[j].getAttribute("data-validator");
-                if (is_blank_validate(inputs[j])) {
+                var dataValidatorValue = inputs[j].getAttribute("data-validator");
+                if (isBlankValidate(inputs[j])) {
                     resetError(inputs[j]);
                 } else {
-                    if (data_validator_value in Validators) {
-                        if (Validators[data_validator_value](inputs[j].value)) {
+                    if (dataValidatorValue in Validators) {
+                        if (Validators[dataValidatorValue](inputs[j].value)) {
                             resetError(inputs[j]);
                         } else {
                             showError(inputs[j]);
